49856c94237958e7956a69006d0c5709105f77df
hiiexchange <146090053+hiiexchange@users.noreply.github.com>
2023-09-27 00:09:42 +0100

    1st

---
+++ tailwind.config.ts
@@ -0,0 +1,44 @@
+import { type Config } from "tailwindcss";
+
+export default {
+  darkMode: ["class"],
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
+    extend: {
+      colors: {
+        background: "#f5f9fe",
+        navbar: "#22295d",
+        "hero-bg": "#363d71",
+        orange: "#f49d3f",
+        "gray-text": "#4d4d4d",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+        spinner: {
+          "0%": { transform: "rotate(0deg)" },
+          "100%": { transform: "rotate(360deg)" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+        spinner: "spinner 1s linear infinite",
+      },
+    },
+  },
+  plugins: [],
+} satisfies Config;
